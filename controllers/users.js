80e3db1f065679820b542fbe77825bc980f18015
lukakhuza <lukakhuza7@gmail.com>
2025-07-28 04:18:41 -0400

    test commit 2

--- controllers/users.js
+++ controllers/users.js
@@ -35,6 +35,8 @@ exports.createUser = (req, res, next) => {
 };
 
 exports.loginUser = (req, res, next) => {
+  console.log("Test 0", req);
+
   const email = req.body.email;
   const password = req.body.password;
   User.findOne({ email: email }).then((user) => {
