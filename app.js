44124dd3677f89c90ccff584e4f165244e7693cf
lukakhuza <lukakhuza7@gmail.com>
2025-07-29 05:29:54 -0400

    added middleware for authentication

--- app.js
+++ app.js
@@ -26,6 +26,16 @@ app.use(bodyParser.json({ strict: false }));
 //   })
 // );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST,PUT,PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const productRoutes = require("./routes/product");
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
