9546f97963befa77efd379afbdc3159f78bb7f52
lukakhuza <lukakhuza7@gmail.com>
2025-08-02 00:36:07 -0400

    test commit 31

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,7 @@ exports.loginUser = (req, res, next) => {
   const password = req.body.password;
   User.findOne({ email: email }).then((user) => {
     if (!user) {
-      const error = new Error("There is no user with the provided email!");
+      const error = Error("There is no user with the provided email!");
       error.statusCode = 401;
       throw error;
     }
