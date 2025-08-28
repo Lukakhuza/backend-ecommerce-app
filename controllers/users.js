ba57b09d8e95cbfdf38ccd9ea238aac23079fe18
lukakhuza <lukakhuza7@gmail.com>
2025-08-28 05:26:42 -0400

    test commit 48

--- controllers/users.js
+++ controllers/users.js
@@ -121,6 +121,7 @@ exports.getUser = (req, res, next) => {
 
 exports.getUserByEmail = (req, res, next) => {
   User.findOne({ email: req.body.email }).then((user) => {
+    console.log("Test 15", user);
     const result = JSON.stringify({
       firstName: user.firstName,
       lastName: user.lastName,
