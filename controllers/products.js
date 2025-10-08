5c352ae7a310dc0b5d0b86f595ab15c9c54cf7ee
lukakhuza <lukakhuza7@gmail.com>
2025-10-08 00:54:58 -0400

    test commit 69

--- controllers/products.js
+++ controllers/products.js
@@ -224,7 +224,8 @@ exports.addToFavorites = (req, res, next) => {
     if (user.favorites.items.includes(productId)) {
       console.log("Includes");
     } else {
-      user.favorites.items.push(parseInt(productId));
+      console.log("Test 20", user.favorites);
+      // user.favorites.items.push(parseInt(productId));
       console.log("Does not include");
     }
 
