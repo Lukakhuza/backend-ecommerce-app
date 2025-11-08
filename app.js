fd8e47d382388846778bb57f5217ef79fc571c9a
lukakhuza <lukakhuza7@gmail.com>
2025-11-08 03:46:31 -0500

    test commit 154

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.7tujvnn.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?w=majority&appName=Cluster1`;
 
-// mongodb+srv://lukakhuza:<db_password>@cluster1.7tujvnn.mongodb.net/?appName=Cluster1
-// mongodb+srv://lukakhuza:<db_password>@cluster1.7tujvnn.mongodb.net/?appName=Cluster1
-
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
