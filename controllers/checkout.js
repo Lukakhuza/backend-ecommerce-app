95420e23deb065f624c187c79d0f62c0b2c958ec
lukakhuza <lukakhuza7@gmail.com>
2025-11-25 06:33:15 -0500

    test commit 167

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,7 +1,8 @@
 const stripe = require("stripe")(process.env.STRIPE_KEY);
+const User = require("../models/user");
 
 exports.addPaymentMethod = async (req, res, next) => {
-  const { paymentMethodId } = req.body;
+  const { paymentMethodId, userId } = req.body;
 
   try {
     // Attach card to customer:
@@ -9,8 +10,24 @@ exports.addPaymentMethod = async (req, res, next) => {
       customer: "cus_TUFLMZpBJmx1YG",
     });
 
+    const newPaymentMethod = {
+      id: attached.id,
+      card: {
+        brand: attached.brand,
+        last4: attached.last4,
+      },
+    };
+
+    // Update mongoDB database:
+    const response = await User.findOneAndUpdate(
+      { _id: userId },
+      { $set: { stripePaymentMethod: newPaymentMethod } }
+    );
+
+    console.log("Response Here: ", response);
+
     // Send card details (id, brand, last4)
-    res.send({ id: attached.id, brand: attached.brand, last4: attached.last4 });
+    // res.send();
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
