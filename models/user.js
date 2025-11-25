01022b051d34e85048a8e115843f51795d1e059d
lukakhuza <lukakhuza7@gmail.com>
2025-11-25 07:21:30 -0500

    test commit 170

--- models/user.js
+++ models/user.js
@@ -51,8 +51,7 @@ const userSchema = new Schema({
   },
   stripePaymentMethod: {
     id: { type: String, required: false },
-    brand: { type: String, required: false },
-    last4: { type: String, required: false },
+    card: { type: Object, required: false },
   },
 });
 
