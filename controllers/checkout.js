011146abf29cf5e2612ff6676e8e425db9ba0f61
lukakhuza <lukakhuza7@gmail.com>
2025-11-25 06:02:14 -0500

    test commit 165

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,4 +1,12 @@
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 exports.addPaymentMethod = async (req, res, next) => {
-  const body = req.body;
-  console.log(body);
+  const { paymentMethodId } = req.body;
+
+  // Attach card to customer:
+  const attached = await stripe.paymentMethods.attach(paymentMethodId, {
+    customer: "cus_TUFLMZpBJmx1YG",
+  });
+
+  console.log("Attached data: ", attached);
 };
