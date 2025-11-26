aae736ec2c1f95b102374c75f8a2f06b5b196193
lukakhuza <lukakhuza7@gmail.com>
2025-11-26 14:40:06 -0500

    test commit 174

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,7 @@ const ordersController = require("../controllers/orders");
 const router = express.Router();
 
 // Create Order
-router.put("/create-order", ordersController.createOrder);
+router.post("/create-order", ordersController.createOrder);
 
 // Fetch orders
 router.get("/fetch-orders/:userId", ordersController.fetchOrders);
